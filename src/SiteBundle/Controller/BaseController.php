f30c3ac1ab3ce5a9928bb33604f4bca732126f5c
Jibe0123 <Jbagostin@gmail.com>
2016-12-23 11:50:24 +0100

    try to fix

--- src/SiteBundle/Controller/BaseController.php
+++ src/SiteBundle/Controller/BaseController.php
@@ -23,25 +23,18 @@ class BaseController extends Controller
             ->add('Password', PasswordType::class)
             ->add('send', SubmitType::class , array(
                 "attr" => array(
-                    "class" => "ink-button green"
-                )
-            ))
+                    "class" => "ink-button green")))
             ->getForm();
 
 
         // le formulaire est recuperer dans la request
         $form->handleRequest($request);
 
-
         //traitement du formulaire
-        if ($form->isSubmitted() && $form->isValid()) {
+        if ($form->isSubmitted() && $form->isValid() && $request->getMethod() == "POST") {
             // data is an array with "name", "email", and "message" keys
             $data = $form->getData();
-
-
             $user = $this->getDoctrine()->getRepository('AchatCentraleCrmBundle:Users')->findBy(array('usMail' => $data['email']));
-
-
             if ($data['Password'] == $user[0]->getUsPass()) {
                 // si toute les infos sont correcte , l'user est connecté
                 //TODO: Collé un cookies a l'utilisateur fraichement ajouté
@@ -121,11 +114,6 @@ class BaseController extends Controller
             $form = $this->get('form.factory')->create(UsersType::class, $user);
 
 
-
-
-
-
-
             // le formulaire est recuperer dans la request
             $form->handleRequest($request);
 
@@ -137,13 +125,7 @@ class BaseController extends Controller
                 $em = $this->getDoctrine()->getManager();
                 $em->persist($data);
                 $em->flush();
-
-
-
             }
-
-
-
 
             return $this->render('@Site/Base/settings.html.twig', array(
                 "user" => $user,
@@ -171,6 +153,9 @@ class BaseController extends Controller
             $response->headers->clearCookie('Is_connected');
             $response->send();
         }
+
+        return $this->redirectToRoute('index');
+
 
     }
 
