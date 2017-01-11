5fe12e8b354029d5925f59583fbd77f0fdbb27ac
Jibe0123 <Jbagostin@gmail.com>
2017-01-11 12:11:41 +0100

    1.0 new client

--- src/AchatCentrale/CrmBundle/Form/ClientsType.php
+++ src/AchatCentrale/CrmBundle/Form/ClientsType.php
@@ -2,7 +2,9 @@
 
 namespace AchatCentrale\CrmBundle\Form;
 
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -17,28 +19,70 @@ class ClientsType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('clRef', TextType::class, array("label" => "La reference client"))
-            ->add('clRaisonsoc', TextType::class, array("label" => "La raison sociale du client"))
-            ->add('clSiret', TextType::class, array("label" => "Le numero de siret"))
-            ->add('clAdresse1', TextType::class, array("label" => "L'adresse du client"))
-            ->add('clAdresse2', TextType::class, array("label" => "L'adresse du client"))
-            ->add('clCp', NumberType::class, array("label" => "Le code postal du client"))
-            ->add('clVille')
-            ->add('clPays')
-            ->add('clTel')
-            ->add('clFax')
-            ->add('clMail')
-            ->add('clWeb')
-            ->add('clLogo')
-            ->add('clPrescript')
-            ->add('clTarif')
-            ->add('clStatus')
-            ->add('clAdhesion')
-            ->add('clActivite')
-            ->add('clGroupe')
-            ->add('clClassif')
-            ->add('clEffectif')
-            ->add('clCa')
+            ->add('clRef', TextType::class, array(
+                "label" => "La reference client",
+                "required" => false
+            ))
+            ->add('clRaisonsoc', TextType::class, array(
+                "label" => "La raison sociale du client",
+                "required" => false
+            ))
+            ->add('clSiret', TextType::class, array(
+                "label" => "Le numero de siret",
+                "required" => false
+            ))
+            ->add('clAdresse1', TextType::class, array(
+                "label" => "L'adresse du client",
+                "required" => false
+            ))
+            ->add('clAdresse2', TextType::class, array(
+                "label" => "L'adresse du client",
+                "required" => false
+            ))
+            ->add('clCp', NumberType::class, array(
+                "label" => "Le code postal du client",
+                "required" => false
+            ))
+            ->add('clVille', TextType::class, array(
+                "label" => "La ville du client",
+                "required" => false
+            ))
+            ->add('clTel', NumberType::class, array(
+                "label" => "Le numero de téléphone du client",
+                "required" => false
+            ))
+            ->add('clFax', NumberType::class, array(
+                "label" => "Le fax du client",
+                "required" => false
+            ))
+            ->add('clMail', EmailType::class, array(
+                "label" => "Le Mail du client",
+                "required" => false
+            ))
+            ->add('clWeb', TextType::class, array(
+                "label" => "Ladresse du site web du client",
+                "required" => false
+            ))
+            ->add('clEffectif', NumberType::class, array(
+                'label' => "L'effectif de l'entreprise",
+                "required" => false
+            ))
+//            ->add('clActivite', EntityType::class, array(
+//                'class' => 'AchatCentrale\CrmBundle\Entity\Activites',
+//                'choice_label' => 'acDescr',
+//            ))
+//            ->add('clGroupe', EntityType::class, array(
+//                'class' => 'AchatCentrale\CrmBundle\Entity\Groupe',
+//                'choice_label' => 'grDescr',
+//            ))
+//            ->add('clPays', EntityType::class, array(
+//                'class' => 'AchatCentrale\CrmBundle\Entity\Pays',
+//                'choice_label' => 'paDescr',
+//            ))
+            ->add('clCa', NumberType::class, array(
+                'label' => "Le chiffre d'affaire de l'entreprise",
+                "required" => false
+            ))
             ->add('Send', SubmitType::class, array(
             "attr" => array(
                 "class" => "ink-button green",
