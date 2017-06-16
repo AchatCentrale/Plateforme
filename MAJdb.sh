8d0a3b09f7ffd612ad4417eec6495e57289a0166
Jibe0123 <Jbagostin@gmail.com>
2017-06-16 15:46:46 +0200

    update

--- MAJdb.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# schema update
-php bin/console doctrine:schema:update --force --no-ansi --dump-sql
-php bin/console generate:doctrine:entities AchatCentraleCrmBundle --no-backup
-php bin/console generate:doctrine:entities SiteBundle --no-backup
-php bin/console doctrine:schema:validate --no-ansi
-
