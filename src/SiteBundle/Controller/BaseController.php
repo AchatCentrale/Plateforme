f034a26b343fa5361d2a665cf561d0378d6255be
Jibe0123 <Jbagostin@gmail.com>
2017-07-10 15:55:20 +0200

    Fix update clients

--- src/SiteBundle/Controller/BaseController.php
+++ src/SiteBundle/Controller/BaseController.php
@@ -517,45 +517,105 @@ class BaseController extends Controller
     {
 
 
-        $em = $this->getDoctrine()->getManager();
+        switch ($centrale) {
+            case "CENTRALE_FUNECAP":
+                $em = $this->getDoctrine()->getManager('centrale_achat_jb');
 
 
-        $siret = $request->request->get('siret');
-        $mail = $request->request->get('mail');
-        $tel = $request->request->get('tel');
-        $cp = $request->request->get('cp');
-        $eff = $request->request->get('eff');
-        $ca = $request->request->get('ca');
-        $adresse = $request->request->get('adresse');
-        $ville = $request->request->get('ville');
+                $siret = $request->request->get('siret');
+                $mail = $request->request->get('mail');
+                $tel = $request->request->get('tel');
+                $cp = $request->request->get('cp');
+                $eff = $request->request->get('eff');
+                $ca = $request->request->get('ca');
+                $adresse = $request->request->get('adresse');
+                $ville = $request->request->get('ville');
 
 
-        $client = $em->getRepository('AchatCentraleCrmBundle:Clients')->findBy([
-            'clId' => $id
-        ]);
+                $client = $em->getRepository('FunecapBundle:Clients:Clients')->findBy([
+                    'clId' => $id
+                ]);
 
-        if (!$client) {
-            throw $this->createNotFoundException(
-                'Pas de client pour l\'id ' . $id
-            );
+
+
+                if (!$client) {
+                    throw $this->createNotFoundException(
+                        'Pas de client pour l\'id ' . $id
+                    );
+                }
+
+                $client[0]->setClSiret($siret);
+                $client[0]->setClMail($mail);
+                $client[0]->setClTel($tel);
+                $client[0]->setClCp($cp);
+                $client[0]->setClEffectif($eff);
+                $client[0]->setClCa($ca);
+                $client[0]->setClAdresse1($adresse);
+                $client[0]->setClVille($ville);
+
+
+                $em->flush();
+
+                $res = "client mise à jour";
+
+
+                return new JsonResponse($res, 200);
+
+                break;
+            case "CENTRALE_ROC_ECLERC":
+                $em = $this->getDoctrine()->getManager();
+
+
+                $siret = $request->request->get('siret');
+                $mail = $request->request->get('mail');
+                $tel = $request->request->get('tel');
+                $cp = $request->request->get('cp');
+                $eff = $request->request->get('eff');
+                $ca = $request->request->get('ca');
+                $adresse = $request->request->get('adresse');
+                $ville = $request->request->get('ville');
+
+
+                $client = $em->getRepository('AchatCentraleCrmBundle:Clients')->findBy([
+                    'clId' => $id
+                ]);
+
+                if (!$client) {
+                    throw $this->createNotFoundException(
+                        'Pas de client pour l\'id ' . $id
+                    );
+                }
+
+                $client[0]->setClSiret($siret);
+                $client[0]->setClMail($mail);
+                $client[0]->setClTel($tel);
+                $client[0]->setClCp($cp);
+                $client[0]->setClEffectif($eff);
+                $client[0]->setClCa($ca);
+                $client[0]->setClAdresse1($adresse);
+                $client[0]->setClVille($ville);
+
+
+                $em->flush();
+
+                $res = "client mise à jour";
+
+
+                return new JsonResponse($res, 200);
+                break;
+            default:
+                break;
         }
 
-        $client[0]->setClSiret($siret);
-        $client[0]->setClMail($mail);
-        $client[0]->setClTel($tel);
-        $client[0]->setClCp($cp);
-        $client[0]->setClEffectif($eff);
-        $client[0]->setClCa($ca);
-        $client[0]->setClAdresse1($adresse);
-        $client[0]->setClVille($ville);
 
 
-        $em->flush();
-
-        $res = "client mise à jour";
 
 
-        return new JsonResponse($res, 200);
+
+
+
+
+
     }
 
     public function newNotesClientAction(Request $request, $id, $centrale)
