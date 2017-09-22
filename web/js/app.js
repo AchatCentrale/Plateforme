426174ec40ee966b68a3b86a1e56124e0bcaff4d
Jibe0123 <Jbagostin@gmail.com>
2017-09-22 12:38:26 +0200

    hastag link tickets

--- web/js/app.js
+++ web/js/app.js
@@ -1606,25 +1606,8 @@ $(function () {
 
         console.log(url);
 
-        swal({
-            title: '<i>HTML</i> <u>example</u>',
-            type: 'info',
-            html:
-            'You can use <b>bold text</b>, ' +
-            '<a href="//github.com">links</a> ' +
-            'and other HTML tags',
-            showCloseButton: true,
-            showCancelButton: true,
-            focusConfirm: false
-            confirmButtonText:
-                '<i class="fa fa-thumbs-up"></i> Great!',
-            confirmButtonAriaLabel: 'Thumbs up, great!',
-            cancelButtonText:
-                '<i class="fa fa-thumbs-down"></i>',
-            cancelButtonAriaLabel: 'Thumbs down',
-        });
 
-
+        swal('Any fool can use a computer')
 
         // $.post( url);
         //
