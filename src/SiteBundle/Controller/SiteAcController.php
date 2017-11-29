1e7688b52e4c2f22d61c7001d52ee73b293b2013
Jibe0123 <Jbagostin@gmail.com>
2017-11-29 10:32:43 +0100

    adhesion v1.0.1

--- src/SiteBundle/Controller/SiteAcController.php
+++ src/SiteBundle/Controller/SiteAcController.php
@@ -10,6 +10,18 @@ class SiteAcController extends Controller
     public function indexAction()
     {
 
+        $conn = $this->get('doctrine.dbal.centrale_achat_jb_connection');
+
+        $sql = "SELECT * FROM CENTRALE_ACHAT.dbo.Vue_All_Taches";
+
+        $stmt = $conn->prepare($sql);
+//        $stmt->bindValue(':id', $id);
+//        $stmt->bindValue(':centrale', $idCentrale);
+
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+        dump($result);
+
         return new Response("ok", 200);
     }
 }
