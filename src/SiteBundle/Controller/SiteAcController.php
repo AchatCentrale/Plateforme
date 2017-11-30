ec74a1a1bf215479690fb710d36a68e27d91a21b
Jibe0123 <Jbagostin@gmail.com>
2017-11-30 10:47:01 +0100

    adhesion

--- src/SiteBundle/Controller/SiteAcController.php
+++ src/SiteBundle/Controller/SiteAcController.php
@@ -5,14 +5,12 @@ namespace SiteBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class SiteAcController extends Controller
 {
     public function indexAction(Request $request)
     {
         header("Access-Control-Allow-Origin: *");
-
 
 
         if ($request->getMethod() == 'POST') {
@@ -30,29 +28,31 @@ class SiteAcController extends Controller
                 ";
 
 
-
-
             $stmt = $conn->prepare($sql);
             $stmt->bindValue(':raison_soc', $CL_RAISONSOC);
             $stmt->bindValue(':tel', $CL_TEL);
-            $stmt->bindValue(':mail',$CL_MAIL);
-            $stmt->bindValue(':ref',"AC-".mt_rand(40000, 99999));
+            $stmt->bindValue(':mail', $CL_MAIL);
+            $stmt->bindValue(':ref', "AC-" . mt_rand(40000, 99999));
 
             $stmt->execute();
             $result = $stmt->fetchAll();
 
 
-
-
-
-
-            return new JsonResponse('ok c\'est enregistrer',200, [
-                'Access-Control-Allow-Origin', 'http://crm.achatcentrale.fr'
-            ] );
-
-
         }
-        return new JsonResponse('fail',200 );
+
+
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'crm.achatcentrale.fr/notification/new/client/ac/'.$CC_NOM.'/'.$CL_MAIL.'/'.$CL_TEL.'/'.$conn->lastInsertId().'');
+        curl_setopt($ch, CURLOPT_POST, true);
+
+        $response = curl_exec($ch);
+
+        return new JsonResponse('ok c\'est enregistrer', 200, [
+            'Access-Control-Allow-Origin', 'http://crm.achatcentrale.fr'
+        ]);
+
+
 
     }
 }
