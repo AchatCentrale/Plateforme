745205be803ee1c43c6256df9fef33c6ab9009e0
Jibe0123 <Jbagostin@gmail.com>
2017-11-30 11:11:27 +0100

    adhesion

--- src/SiteBundle/Controller/SiteAcController.php
+++ src/SiteBundle/Controller/SiteAcController.php
@@ -42,11 +42,8 @@ class SiteAcController extends Controller
 
 
 
-        $mailer = $this->get('site.service.client_services');
 
-        $mailer->sendNotifNewClientAc($CC_NOM, $CL_MAIL, $CL_TEL, $conn->lastInsertId());
-
-        return new JsonResponse('ok c\'est enregistrer', 200, [
+        return new JsonResponse('crm.achatcentrale.fr/notification/new/client/ac/'.$CC_NOM.'/'.$CL_MAIL.'/'.$CL_TEL.'/'.$conn->lastInsertId().'', 200, [
             'Access-Control-Allow-Origin', 'http://crm.achatcentrale.fr'
         ]);
 
