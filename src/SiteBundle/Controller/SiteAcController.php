327a23c160e4cdcc0d88078ccd89e20daff1bde8
Jibe0123 <Jbagostin@gmail.com>
2018-03-26 12:49:24 +0200

    logo mails

--- src/SiteBundle/Controller/SiteAcController.php
+++ src/SiteBundle/Controller/SiteAcController.php
@@ -26,9 +26,9 @@ class SiteAcController extends Controller
                     INSERT INTO CENTRALE_ACHAT.dbo.CLIENTS (SO_ID, RE_ID, CL_REF,CL_STATUS, CL_RAISONSOC, CL_MAIL, CL_ADHESION , CL_ACTIVITE, CL_PRESCRIPT, CL_TARIF, CL_CLASSIF, INS_DATE, INS_USER, MAJ_DATE, MAJ_USER   )
                     VALUES
                       (1, 1, :ref, 0 , :raison_soc, :mail, 'REGARDE', 0, 0, 0, 0, GETDATE(), 'SITE-AC',GETDATE(), 'SITE-AC' )
-                    INSERT INTO CENTRALE_ACHAT.dbo.CLIENTS_USERS (CL_ID, PU_ID, CC_PRENOM, CC_FONCTION, CC_TEL, CC_PASS, CC_NIVEAU, CC_STATUS, INS_DATE, INS_USER, MAJ_DATE, MAJ_USER)
+                    INSERT INTO CENTRALE_ACHAT.dbo.CLIENTS_USERS (CL_ID, PU_ID, CC_NOM, CC_FONCTION, CC_TEL, CC_PASS, CC_NIVEAU, CC_STATUS, INS_DATE, INS_USER, MAJ_DATE, MAJ_USER)
                     VALUES
-                      (scope_identity(), 1, :prenom, 'Gérant/Président', :tel, :pass, 1, 0, GETDATE(), 'SITE-AC',GETDATE(), 'SITE-AC' )
+                      (scope_identity(), 1, :nom, 'Gérant/Président', :tel, :pass, 1, 0, GETDATE(), 'SITE-AC',GETDATE(), 'SITE-AC' )
                     COMMIT
                 ";
 
@@ -37,26 +37,26 @@ class SiteAcController extends Controller
             $stmt->bindValue(':raison_soc', $CL_RAISONSOC);
             $stmt->bindValue(':tel', $CL_TEL);
             $stmt->bindValue(':mail', $CL_MAIL);
-            $stmt->bindValue(':prenom', $CC_NOM);
+            $stmt->bindValue(':nom', $CC_NOM);
             $stmt->bindValue(':ref', "AC-" . mt_rand(40000, 99999));
             $stmt->bindValue(':pass', $token);
 
             $stmt->execute();
             $result = $stmt->fetchAll();
 
+            $urls = [
+                "ac" => "http://crm.achatcentrale.fr/notification/new/client/ac/".$CC_NOM."/".$CL_MAIL."/".$CL_TEL."/".$conn->lastInsertId(),
+                "client" => "http://crm.achatcentrale.fr/notification/new/client/client/" . $CC_NOM . "/" . $CL_RAISONSOC . "/" . $CL_MAIL . "/" . $CL_TEL . ""
+            ];
+
+
+            return new JsonResponse($urls, 200, [
+                'Access-Control-Allow-Origin', 'http://crm.achatcentrale.fr'
+            ]);
 
         }
 
-        $urls = [
-            "ac" => "http://crm.achatcentrale.fr/notification/new/client/ac/".$CC_NOM."/".$CL_MAIL."/".$CL_TEL."/".$conn->lastInsertId(),
-            "client" => "http://crm.achatcentrale.fr/notification/new/client/client/" . $CC_NOM . "/" . $CL_RAISONSOC . "/" . $CL_MAIL . "/" . $CL_TEL . ""
-        ];
-
-
-        return new JsonResponse($urls, 200, [
-            'Access-Control-Allow-Origin', 'http://crm.achatcentrale.fr'
-        ]);
-
+        return $this->redirect('http://www.achatcentrale.fr/', 301);
 
     }
 }
