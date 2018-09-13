9b7eed0e1d7ddf672babbd21883b412711fead1a
Jibe0123 <Jbagostin@gmail.com>
2018-09-13 16:19:58 +0200

    detail notes homes

--- web/js/app.js
+++ web/js/app.js
@@ -972,7 +972,7 @@ $(function () {
 
 
                 let tpl = `<div class="ui centered grid header-detail-note">
-                              <h4>Créée le <b>${data.ins_date}</b> pour le client <b>${data.cl_raisonsoc.CL_RAISONSOC}</b> par <b>${data.ins_user}</b></h4>
+                              <h4>Créée le <b>${data.ins_date}</b> pour le client <b>${data.cl_raisonsoc}</b> par <b>${data.ins_user}</b></h4>
                             <div class="one column row tpl-inject-note">
                                 <div class="column note-content-home">
                                     <p class="note-description">${data.nom}</p>
