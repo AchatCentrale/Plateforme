880ac7fa572a5d123cb985cc8d81289ec5b82002
Jibe0123 <Jbagostin@gmail.com>
2018-10-01 11:58:25 +0200

    Add debug for import product

--- src/SiteBundle/Controller/FournisseurController.php
+++ src/SiteBundle/Controller/FournisseurController.php
@@ -155,7 +155,7 @@ class FournisseurController extends Controller
                         switch ($ligne[41]) {
                             case "A RAJOUTER":
 
-                                $descr = iconv("UTF-8", "French_CI_AS", $ligne[28]);
+                                $descr = iconv("UTF-8", "WINDOWS-1252", $ligne[28]);
 
                                 $sql = "INSERT INTO CENTRALE_PRODUITS.dbo.IMPORT_PRODUITS (PART_ID, Fournisseur, Rayon, Famille, Filtre1, Valeur1, Filtre2, Valeur2, Filtre3, Valeur3, Filtre4, Valeur4, Filtre5, Valeur5, Filtre6, Valeur6, Filtre7, Valeur7, Filtre8, Valeur8, Filtre9, Valeur9, Filtre10, Valeur10, Ref_Fourn, Ref_Part, EAN, Nom_Produit, Descrip_Courte, Descrip_Longue, Triptyque, Qte_Cmde, Conditionnement, Prix_Public_HT, Prix_Part_HT, Prix_VC, Remise_PCT, Type_Lien, Lien, Photo, Variable_Session)
                             VALUES
