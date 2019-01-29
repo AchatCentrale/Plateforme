87e908842cf37d046ccfc90efefcf629df5bd817
Jibe0123 <Jbagostin@gmail.com>
2019-01-29 12:05:45 +0100

    Detail taches a revoir

--- web/js/Count.js
+++ web/js/Count.js
@@ -4,8 +4,7 @@ Countable.on(area, counter =>{
     if(counter.all <= 500){
         $('#counting-note').empty().append('<p>'+counter.all+'/500</p>');
     }else{
-        $("#add-note").prop('disabled', true);
-        $('#counting-note').empty().append('<p>'+counter.all+'/500</p>');
+        $('#counting-note').empty().append('<p style="color: red;">'+counter.all+'/500</p>');
 
 
     }
